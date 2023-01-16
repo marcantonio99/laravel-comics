41169be9712223d85c2d5199a6220cf124fdfc87
marcantonio99 <marcantonio8799@gamil.com>
2023-01-16 17:04:15 +0100

    start

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,11 @@
+<nav>
+    <img src="" alt="">
+    <ul>
+        <li>
+            <a href="">Home</a>
+        </li>
+        <li>
+            <a href="">Comics</a>
+        </li>
+    </ul>
+</nav>
